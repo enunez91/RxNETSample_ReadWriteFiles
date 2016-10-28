f9ae0ccf17fe2fd70d1b3e49b8983d4438a063ca
enunez_91 <eduardodaniel.03@gmail.com>
2016-10-27 21:57:39 -0400

    Commit init

--- RxNETSample_ReadWriteFiles/Form1.Designer.cs
+++ RxNETSample_ReadWriteFiles/Form1.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridFile = new System.Windows.Forms.DataGridView();
+            this.Line = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnReadFile = new System.Windows.Forms.Button();
+            this.txtFileToRead = new System.Windows.Forms.TextBox();
+            this.fileDialog = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridFile)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridFile
+            // 
+            this.dataGridFile.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridFile.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Line});
+            this.dataGridFile.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridFile.Location = new System.Drawing.Point(0, 0);
+            this.dataGridFile.Name = "dataGridFile";
+            this.dataGridFile.Size = new System.Drawing.Size(436, 150);
+            this.dataGridFile.TabIndex = 0;
+            // 
+            // Line
+            // 
+            this.Line.HeaderText = "Line file";
+            this.Line.Name = "Line";
+            // 
+            // btnReadFile
+            // 
+            this.btnReadFile.Location = new System.Drawing.Point(349, 166);
+            this.btnReadFile.Name = "btnReadFile";
+            this.btnReadFile.Size = new System.Drawing.Size(75, 23);
+            this.btnReadFile.TabIndex = 1;
+            this.btnReadFile.Text = "Read";
+            this.btnReadFile.UseVisualStyleBackColor = true;
+            // 
+            // txtFileToRead
+            // 
+            this.txtFileToRead.Location = new System.Drawing.Point(12, 169);
+            this.txtFileToRead.Name = "txtFileToRead";
+            this.txtFileToRead.Size = new System.Drawing.Size(331, 20);
+            this.txtFileToRead.TabIndex = 2;
+            this.txtFileToRead.Text = "c:\\temp\\test.txt";
+            // 
+            // fileDialog
+            // 
+            this.fileDialog.FileName = "Open";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(436, 211);
+            this.Controls.Add(this.txtFileToRead);
+            this.Controls.Add(this.btnReadFile);
+            this.Controls.Add(this.dataGridFile);
+            this.Name = "Form1";
+            this.Text = "Read and write to file with Rx.NET";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridFile)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridFile;
+        private System.Windows.Forms.Button btnReadFile;
+        private System.Windows.Forms.TextBox txtFileToRead;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Line;
+        private System.Windows.Forms.OpenFileDialog fileDialog;
     }
 }
 
--- RxNETSample_ReadWriteFiles/Form1.cs
+++ RxNETSample_ReadWriteFiles/Form1.cs
@@ -7,14 +7,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive;
+using System.Reactive.PlatformServices;
+using System.Reactive.Concurrency;
 
 namespace RxNETSample_ReadWriteFiles
 {
     public partial class Form1 : Form
     {
+        private IObservable<EventPattern<EventArgs>> read;
+        private IObserver<EventPattern<EventArgs>> ob;
+
         public Form1()
         {
             InitializeComponent();
+            createEventHandlerToBtnRead();
+        }
+
+        private void createEventHandlerToBtnRead()
+        {
+            ob = Observer.Create<EventPattern<EventArgs>>(o => 
+            {
+                if (File.Exists(txtFileToRead.Text))
+                {
+                    string[] lines = File.ReadAllLines(txtFileToRead.Text);
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        dataGridFile.Rows.Add(lines[i]);
+                        Thread.Sleep(1000);
+                    }
+                }
+            });
+
+            read = Observable.FromEventPattern<EventArgs>(btnReadFile, "Click");
+            read.Subscribe(ob);
         }
     }
 }
