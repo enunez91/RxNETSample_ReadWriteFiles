78fef16bade2a45a48d1b38b4cb4094a0a799b64
enunez_91 <eduardodaniel.03@gmail.com>
2016-10-29 18:10:37 -0400

    Add scheduler to

--- RxNETSample_ReadWriteFiles/Form1.cs
+++ RxNETSample_ReadWriteFiles/Form1.cs
@@ -12,40 +12,72 @@ using System.Threading;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Reactive;
-using System.Reactive.PlatformServices;
 using System.Reactive.Concurrency;
+
 
 namespace RxNETSample_ReadWriteFiles
 {
     public partial class Form1 : Form
     {
-        private IObservable<EventPattern<EventArgs>> read;
-        private IObserver<EventPattern<EventArgs>> ob;
-
         public Form1()
         {
             InitializeComponent();
-            createEventHandlerToBtnRead();
+            attachUIEvents();
         }
 
-        private void createEventHandlerToBtnRead()
+        private void attachUIEvents()
         {
-            ob = Observer.Create<EventPattern<EventArgs>>(o => 
+            var cancel = new CancellationDisposable();
+
+            var scheduler = new NewThreadScheduler(t => new Thread(t) { Name = "Thread1" });
+            var gridSch = new ControlScheduler(dataGridFile);
+            
+            var ob = Observer.Create<EventPattern<object>>(o => 
             {
-                if (File.Exists(txtFileToRead.Text))
+                //wait a moment.
+                //non blocking
+                Thread.Sleep(5000);
+
+                string[] lines = getLinesFromFile();
+
+                if(lines != null)
                 {
-                    string[] lines = File.ReadAllLines(txtFileToRead.Text);
-                    for (int i = 0; i < lines.Length; i++)
+                    lines.ToObservable().ObserveOn(gridSch)
+                    .Subscribe(l =>
                     {
-                        dataGridFile.Rows.Add(lines[i]);
-                        Thread.Sleep(2000); 
-                    }
-                }
+                        addLineToGrid(l);
+                    });
+                }                
             });
 
-            read = Observable.FromEventPattern<EventArgs>(btnReadFile, "Click");
-            read.Subscribe(ob);
-            ob.OnCompleted();
+            Observable.FromEventPattern(
+                h => this.btnReadFile.Click += h,
+                h => this.btnReadFile.Click -= h)
+                .ObserveOn(scheduler)
+                .Subscribe(ob, cancel.Token);
+
+            Observable.FromEventPattern(
+                h => Application.ApplicationExit += h,
+                h => Application.ApplicationExit -= h)
+                .Subscribe( _ => {
+                        cancel.Dispose();
+                    });      
+                       
+        }
+
+        private string[] getLinesFromFile()
+        {
+            if (File.Exists(txtFileToRead.Text))
+            {
+                string[] lines = File.ReadAllLines(txtFileToRead.Text);                
+                return lines;
+            }
+            return null;
+        }
+
+        private void addLineToGrid(string line)
+        {
+            dataGridFile.Rows.Add(line);            
         }
     }
 }
